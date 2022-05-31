f233c803348cca3004e7e87665654808dd184197
zami tulande <hulmartulande@gmail.com>
2022-05-31 08:10:41 -0500

    contador atras

--- js/app.js
+++ js/app.js
@@ -1,97 +1,111 @@
-
-const Carrito = document.querySelector('#carrito');
-const tablaDeCarrito = document.querySelector('#lista-carrito tbody');
-const btnVaciarCarrito = document.querySelector('#vaciar-carrito');
-const ListaDeCursos = document.querySelector('#lista-cursos');
-const contador = document.querySelector('#contador');
+const Carrito = document.querySelector("#carrito");
+const tablaDeCarrito = document.querySelector("#lista-carrito tbody");
+const btnVaciarCarrito = document.querySelector("#vaciar-carrito");
+const ListaDeCursos = document.querySelector("#lista-cursos");
+const contador = document.querySelector("#contador");
 let llenandoCarrito = [];
 
 registrarEvento();
 
-function registrarEvento(){
-    ListaDeCursos.addEventListener('click', seleccionandoCurso);
-    Carrito.addEventListener('click', eliminarCurso);
-    btnVaciarCarrito.addEventListener('click', () =>{
-        llenandoCarrito = [];
-         bgWhite ();
-         limpiarCarritoPrevio();
-         eliminarVista();
-    });
-}
-function seleccionandoCurso(e){
-    e.preventDefault();
-    if(e.target.classList.contains('agregar-carrito')){
-        const cursoSeleccionado = e.target.parentElement.parentElement;
-        leerDatosCursoSeleccionado(cursoSeleccionado);
-    }
-};
-
-function eliminarCurso (e){
-    const claseBorrar = e.target.classList.contains('borrar-curso');
-    if(claseBorrar){
-        const cursoABorrar = e.target.getAttribute('data-id');
-
-        llenandoCarrito = llenandoCarrito.filter(cursoParaBorrar => cursoParaBorrar.idDeCurso !== cursoABorrar);
-        bgWhite ();   
-        generarHtmlEnCarrito();
-        
-    };
-}
-
-function leerDatosCursoSeleccionado(cursoSeleccionado){
-    const infoCursoSeleccionado={
-        imagenDeCurso: cursoSeleccionado.querySelector('img').src,
-        tituloDeCurso: cursoSeleccionado.querySelector('h4').textContent,
-        precioDeCurso: cursoSeleccionado.querySelector('.precio span').textContent,
-        idDeCurso: cursoSeleccionado.querySelector('a').getAttribute('data-id'),
-        cantidad: 1
-    };
-    const cursosExistentes = llenandoCarrito.some(cursoExistente =>cursoExistente.idDeCurso === infoCursoSeleccionado.idDeCurso);
-    if(cursosExistentes){
-        //actializar cantidad
-        const verCursosExistentes = llenandoCarrito.map(verCursoExistente => {
-            if(verCursoExistente.idDeCurso === infoCursoSeleccionado.idDeCurso){
-               verCursoExistente.cantidad++
-                 
-                return verCursoExistente;
-            }else{
-                return verCursoExistente;
-            }
-        });
-        llenandoCarrito =[...verCursosExistentes];
-        
-    }else{
-        //agregar curso al carrito
-        llenandoCarrito = [...llenandoCarrito, infoCursoSeleccionado];
-
-    }
-    bgred();   
-
-    generarHtmlEnCarrito();
-    
-}
-function bgred (){
-    const red = llenandoCarrito.length;
-    if(red >= 0){
-        const bg = document.querySelector("#img-carrito");
-        bg.style.backgroundColor='#1EAEDB';        
-    }
-}
-function bgWhite (){
-    if(llenandoCarrito.length === 0){
-        const bgw= document.querySelector("#img-carrito");
-        bgw.style.backgroundColor='white';
-    };
-}
-
-function generarHtmlEnCarrito(){
-
+function registrarEvento() {
+  ListaDeCursos.addEventListener("click", seleccionandoCurso);
+  Carrito.addEventListener("click", eliminarCurso);
+  btnVaciarCarrito.addEventListener("click", () => {
+    llenandoCarrito = [];
+    bgWhite();
     limpiarCarritoPrevio();
+    eliminarVista();
+  });
+}
+function seleccionandoCurso(e) {
+  e.preventDefault();
+  if (e.target.classList.contains("agregar-carrito")) {
+    const cursoSeleccionado = e.target.parentElement.parentElement;
+    leerDatosCursoSeleccionado(cursoSeleccionado);
+  }
+}
 
-    llenandoCarrito.forEach(cursoComprado=>{
-        const  {imagenDeCurso, tituloDeCurso, precioDeCurso, cantidad}  = cursoComprado;
-        const tr = document.createElement('tr');
-        tr.innerHTML=  ` 
+function eliminarCurso(e) {
+  const claseBorrar = e.target.classList.contains("borrar-curso");
+  if (claseBorrar) {
+    const cursoABorrar = e.target.getAttribute("data-id");
+
+    llenandoCarrit = llenandoCarrito.some(
+      (cursoParaBorrar) => cursoParaBorrar.idDeCurso === cursoABorrar
+    );
+    if (llenandoCarrit) {
+      const eliminar = llenandoCarrito.map((elimin) => {
+        if (elimin.idDeCurso === cursoABorrar) {
+          elimin.cantidad--;
+          const limite = 0;
+          if (elimin.cantidad === limite) {
+            llenandoCarrito = llenandoCarrito.filter(
+              (cursoParaBorrar) => cursoParaBorrar.idDeCurso !== cursoABorrar
+            );
+          }
+          bgWhite();
+          generarHtmlEnCarrito();
+        }
+      });
+    }
+  }
+}
+
+function leerDatosCursoSeleccionado(cursoSeleccionado) {
+  const infoCursoSeleccionado = {
+    imagenDeCurso: cursoSeleccionado.querySelector("img").src,
+    tituloDeCurso: cursoSeleccionado.querySelector("h4").textContent,
+    precioDeCurso: cursoSeleccionado.querySelector(".precio span").textContent,
+    idDeCurso: cursoSeleccionado.querySelector("a").getAttribute("data-id"),
+    cantidad: 1,
+  };
+  const cursosExistentes = llenandoCarrito.some(
+    (cursoExistente) =>
+      cursoExistente.idDeCurso === infoCursoSeleccionado.idDeCurso
+  );
+  if (cursosExistentes) {
+    //actializar cantidad
+    const verCursosExistentes = llenandoCarrito.map((verCursoExistente) => {
+      if (verCursoExistente.idDeCurso === infoCursoSeleccionado.idDeCurso) {
+        verCursoExistente.cantidad++;
+
+        return verCursoExistente;
+      } else {
+        return verCursoExistente;
+      }
+    });
+    llenandoCarrito = [...verCursosExistentes];
+  } else {
+    //agregar curso al carrito
+    llenandoCarrito = [...llenandoCarrito, infoCursoSeleccionado];
+  }
+  bgred();
+
+  generarHtmlEnCarrito();
+}
+function bgred() {
+  const red = llenandoCarrito.length;
+  if (red >= 0) {
+    const bg = document.querySelector("#img-carrito");
+    bg.style.backgroundColor = "#1EAEDB";
+  }
+}
+function bgWhite() {
+  if (llenandoCarrito.length === 0) {
+    const bgw = document.querySelector("#img-carrito");
+    bgw.style.backgroundColor = "white";
+  }
+}
+
+function generarHtmlEnCarrito() {
+  limpiarCarritoPrevio();
+
+  llenandoCarrito.forEach((cursoComprado) => {
+    const { imagenDeCurso, tituloDeCurso, precioDeCurso, cantidad } =
+      cursoComprado;
+    const tr = document.createElement("tr");
+    tr.setAttribute("id", "campo");
+    tr.innerHTML = ` 
             <td>
                 <img src="${imagenDeCurso}" width="100"/>
             </td>
@@ -110,41 +124,41 @@ function generarHtmlEnCarrito(){
             
         `;
 
-        tablaDeCarrito.appendChild(tr);
-    }); 
-    contadores();
+    tablaDeCarrito.appendChild(tr);
+  });
+  contadores();
 }
-function limpiarCarritoPrevio(){
-    while(tablaDeCarrito.firstChild){
-        const r = tablaDeCarrito.removeChild(tablaDeCarrito.firstChild);               
-    }    
+function limpiarCarritoPrevio() {
+  while (tablaDeCarrito.firstChild) {
+    const r = tablaDeCarrito.removeChild(tablaDeCarrito.firstChild);
+  }
 }
-function eliminarVista(){
-    while(contador.firstChild){
-        const f = contador.removeChild(contador.firstChild);
-    }
+function eliminarVista() {
+  while (contador.firstChild) {
+    const f = contador.removeChild(contador.firstChild);
+  }
 }
-function contadores(){
-    let t = llenandoCarrito.map(cantidad=>{
-        let suma = cantidad.cantidad;  
-        return suma;
-    })   
-    const array = t;
-    let sum = 0;
+function contadores() {
+  let t = llenandoCarrito.map((cantidad) => {
+    let suma = cantidad.cantidad;
+    return suma;
+  });
+  const array = t;
+  let sum = 0;
 
-    for (let i = 0; i < array.length; i++) {
+  for (let i = 0; i < array.length; i++) {
     sum += array[i];
-}
-    eliminarVista();
-    vistaContador(sum);
+  }
+  eliminarVista();
+  vistaContador(sum);
 }
 
-function vistaContador(sum){
-    if(sum > 0){
-        const p= document.createElement('span');
-    p.innerHTML=` 
-        ${ '+' +sum}
+function vistaContador(sum) {
+  if (sum > 0) {
+    const p = document.createElement("span");
+    p.innerHTML = ` 
+        ${"+" + sum}
     `;
     contador.appendChild(p);
-     }   
+  }
 }
